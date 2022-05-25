18b5bddcb1ce90eee1ca84573783c800519860cb
Lisauskaite <vismante.lisauskaite@gmail.com>
2022-05-25 22:31:47 +0300

    Assert if product icon is visible

--- src/test/java/tests/receptorius/GryniejiPrieskoniai.java
+++ src/test/java/tests/receptorius/GryniejiPrieskoniai.java
@@ -1,5 +1,6 @@
 package tests.receptorius;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.BaseTests;
@@ -7,14 +8,19 @@ import tests.BaseTests;
 public class GryniejiPrieskoniai extends BaseTests {
     @BeforeMethod
     public void openSpicePage() {
+
         pages.receptorius.GryniejiPrieskoniai.openLoginUrl();
     }
+
     @Test
-    public void searchForSpicesWithSearchBar (){
+    public void searchForSpicesWithSearchBar() {
+
         String spiceName = "Deggi Mirch";
+
         pages.receptorius.GryniejiPrieskoniai.addSpiceNameToSearchBar(spiceName);
         pages.receptorius.GryniejiPrieskoniai.clickSearchButton();
         pages.receptorius.GryniejiPrieskoniai.checkIfElementIsVisible();
 
+        Assert.assertTrue(pages.receptorius.GryniejiPrieskoniai.checkIfElementIsVisible());
     }
 }
