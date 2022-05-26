35867cca4f5554f0a1b0707ff5b33c85c7b37cb0
Lisauskaite <vismante.lisauskaite@gmail.com>
2022-05-26 15:50:25 +0300

    FIX - switched around test to prevent failure in addAmountThatIsMoreThanThereIsInStorage

--- src/test/java/tests/receptorius/AddToCart.java
+++ src/test/java/tests/receptorius/AddToCart.java
@@ -11,7 +11,22 @@ public class AddToCart extends BaseTests {
     public void openPage() {
         pages.receptorius.AddToCart.open();
     }
+    @Test
+    public void addAmountThatIsMoreThanThereIsInStorage(){
 
+        String largeNumberAmount = "10";
+        String expectedTextInToManyItemsAlertMessage = "Pageidaujamo kiekio";
+
+        pages.receptorius.AddToCart.click25kgButton();
+        pages.receptorius.AddToCart.clearValueInInput();
+        pages.receptorius.AddToCart.addNewAmountOfItems(largeNumberAmount);
+        pages.receptorius.AddToCart.clickAddToCartButton();
+        pages.receptorius.AddToCart.waitForAlertMesssageToBeVisible();
+        String actualToManyItemsAlertMessage = pages.receptorius.AddToCart.readNotEnoughItemsAvailableAlertMessage();
+
+        Assert.assertTrue(actualToManyItemsAlertMessage.contains(expectedTextInToManyItemsAlertMessage));
+
+    }
     @Test
     public void addItemsToCart() {
         String expectedMessageSuccessfullyAddedToCartMessage = "Prekė sėkmingai pridėta";
@@ -29,22 +44,6 @@ public class AddToCart extends BaseTests {
         String actualNumberOfItemsInShoppingCartIcon = pages.receptorius.AddToCart.getNumberOfItemsInCartIcon();
 
         Assert.assertTrue(actualNumberOfItemsInShoppingCartIcon.contains(expectedNumberOfItemsInShoppingCartIcon));
-
-    }
-    @Test
-    public void addAmountThatIsMoreThanThereIsInStorage(){
-
-        String largeNumberAmount = "5";
-        String expectedTextInToManyItemsAlertMessage = "Pageidaujamo kiekio";
-
-        pages.receptorius.AddToCart.click25kgButton();
-        pages.receptorius.AddToCart.clearValueInInput();
-        pages.receptorius.AddToCart.addNewAmountOfItems(largeNumberAmount);
-        pages.receptorius.AddToCart.clickAddToCartButton();
-        pages.receptorius.AddToCart.waitForAlertMesssageToBeVisible();
-        String actualToManyItemsAlertMessage = pages.receptorius.AddToCart.readNotEnoughItemsAvailableAlertMessage();
-
-        Assert.assertTrue(actualToManyItemsAlertMessage.contains(expectedTextInToManyItemsAlertMessage));
 
     }
 
