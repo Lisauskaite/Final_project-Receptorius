e19c05de3c7d66ead4658be013eacb204d4cc358
Lisauskaite <vismante.lisauskaite@gmail.com>
2022-05-26 14:32:38 +0300

    Added screen maximizing on window opening

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,13 +3,16 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver() {
